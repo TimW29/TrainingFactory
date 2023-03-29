bb16f21426cef1774e79f3a18f278ae67f6b7799
TimW29 <timwijnandswerk@gmail.com>
2023-03-29 14:19:42 +0200

    email

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'timwijnands24@gmail.com';
+    public string $fromName   = 'Tim';
     public string $recipients = '';
 
     /**
